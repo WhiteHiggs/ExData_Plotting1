150e32f5c93d8e93af71bd7bc29816053594f12e
WhiteHiggs <WhiteHiggs@users.noreply.github.com>
2015-09-11 17:20:33 +0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -14,44 +14,42 @@ timedata<- read.table("household_power_consumption.txt",
 ##  Subset column number from the dates 2007-02-01 and 2007-02-02
 colnum<-which(timedata$Date==c('1/2/2007') | timedata$Date==c('2/2/2007'))
 
-## Read in the data only contain column 3 which represents Global_active_power
+## Read in the data containing column 2(Time), 7(Sub_metering_1), 8(Sub_metering_2), 9(Sub_metering_3)
 powerdata<- read.table("household_power_consumption.txt",
                        comment.char = "#",
                        header = TRUE,
                        sep = ";",
                        na.strings = "",
-                       colClasses = c("NULL", rep("factor",4),"NULL",rep("factor",3)))
+                       colClasses = c("NULL", "factor",rep("NULL",4),"factor","factor","factor"))
 
-## Subset Global_active_power data from the dates 2007-02-01 and 2007-02-02
+## Subset Sub_metering data from the dates 2007-02-01 and 2007-02-02
 sub1_factor<-powerdata$Sub_metering_1[colnum]
 sub2_factor<-powerdata$Sub_metering_2[colnum]
 sub3_factor<-powerdata$Sub_metering_3[colnum]
-GAP_factor<-powerdata$Global_active_power[colnum]
-V_factor<-powerdata$Voltage[colnum]
-GRP_factor<-powerdata$Global_reactive_power[colnum]
 
-
+## Combine data of Date and Time
 day_factor<-timedata$Date[colnum]
 time_factor<-powerdata$Time[colnum]
 timedata<-paste(day_factor,time_factor)
+##  Convert the Date and Time variables to Date/Time classes
+day<-strptime(timedata, "%d/%m/%Y %H:%M:%S")
 
-## Convert the Global_Active_Power data to numeric
+
+## Convert the Sub_metering data data to numeric
 sub1_numeric<-as.numeric(as.character(sub1_factor))
 sub2_numeric<-as.numeric(as.character(sub2_factor))
 sub3_numeric<-as.numeric(as.character(sub3_factor))
 
-V_numeric<-as.numeric(as.character(V_factor))
-
-GRP_numeric<-as.numeric(as.character(GRP_factor))
-GAP_numeric<-as.numeric(as.character(GAP_factor))
-
-par(mfrow=c(2,2))
-plot(day,GAP_numeric,type = "l", ylab = "Global Active Power (kilowatts)",xlab = "")
-plot(day,V_numeric,type = "l", ylab = "Voltage",xlab = "datetime")
-
+## Construct plot3
+## In order to make the appropriate location of the legend in a 480*480 png file,
+## parameters like "adj", "x.intersp", "y.intersp" are specially set.
 plot(day,sub1_numeric,type = "l", ylab = "Energy sub metering",xlab = "")
 lines(day,sub2_numeric,col="red")
 lines(day,sub3_numeric,col="blue")
-legend("topright",lty=1, c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),bty="n")
+legend("topright",lty=1,legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       col=c("black","red","blue"),adj = c(0.2,0.4),
+       x.intersp = 3,y.intersp = 0.5)
 
-plot(day,GRP_numeric,type = "l", ylab = "Global_reactive_power",xlab = "datetime")
+## Save plot files as plot3.png
+dev.copy(png, file = "plot3.png",width=480,height=480)
+dev.off()
